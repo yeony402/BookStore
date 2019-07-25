67c4cdf56f894b0953b7b85f10caef635153ba60
yeony402 <yeony402@naver.com>
2019-07-25 20:06:52 +0900

    온라인 서점 크롤링에서 api사용으로 변경 - 해당결과 리스트로 출력 확인

--- bookstore.py
+++ bookstore.py
@@ -1,43 +1,51 @@
-import xlrd, xlwt
+import sys
+import xlrd
 import requests
 from urllib.request import quote
 import urllib.parse
 from bs4 import BeautifulSoup as bs
+import mysql.connector
 import pandas as pd
 
-# 엑셀파일 읽어오기
-wb = xlrd.open_workbook('C:/Users/yeony/Desktop/test_excel.xlsx')
-# 모든 시트 가져오기
-sheets = wb.sheets()
-# 모든 시트 디비로 저장
-## 첫번째 시트만 가져오기
-# sheet = wb.sheet_by_index(0)
-list = []
 
-for sheet in sheets:
-    for i in range(1, sheet.nrows):
-        a1 = sheet.cell_value(i, 0)
-        a2 = sheet.cell_value(i, 1)
-        list.append(str(a1)+str(a2))
+##########################################################
+# query = sys.argv[1]
+
+# 알리딘 api로 가져올 데이터
+data = urllib.parse.urlencode({
+	'ttbkey' : 'ttbyeony4022121001',
+    'SearchTarget':'Book',
+    'Query':'코딩인터뷰 완전분석 게일라크만',
+    'QueryType':'Keyword',
+    'Sort':'Title',
+})
+# 검색api url
+url = 'http://www.aladdin.co.kr/ttb/api/ItemSearch.aspx'
+con = urllib.request.urlopen(url +'?' + data)
+html = con.read()
+con.close()
+
+# 요청한 값 lxml로 읽어오기
+soup = bs(html, "lxml")
+soupString = str(soup)
+
+list=[]
+stock_list=[]
+if soupString.find('<error xml') > -1:
+	for s in soup('errormessage'):
+		print ('## Error!! ##')
+		print ("Error Message: ")
+else:
+    # 도서별로 리스트에 담기
+    for title in soup('title')[1] :
+        stock_list.append(title)
+        for author in soup('author')[1]:
+            stock_list.append(author)
+            for publisher in soup('publisher')[1]:
+                stock_list.append(publisher)
+                for pricesales in soup('pricesales')[1]:
+                    stock_list.append(pricesales)
+                    for stockstatus in soup('stockstatus')[1]:
+                        stock_list.append(stockstatus)
+    list.append(stock_list)
     print(list)
-
-
-
-url = 'http://www.yes24.co.kr/'
-data = {'query' : '뻘짓은 나만 하는줄 알았어 피터홀린스'}
-# data = unquote(data).decode('utf8')
-response = requests.post(url, data=data)
-print(response)
-
-
-aa = '%C3%D6%B0%AD%C0%C7%C0%CE%BB%FD'
-aa = urllib.parse.unquote_plus(aa)
-print(aa)
-
-bb = '최강의 강의'
-bb = urllib.parse.quote_plus(bb)
-print(bb)
-
-# yes24에 '최강의 강의'를 검색했을 때 url에 나타나는 암호를
-# aa와 같이 디코딩했을 때 결과 - �ְ����λ�
-# '최강의 강의'를 인코딩했을 때 결과 - %EC%B5%9C%EA%B0%95%EC%9D%98+%EA%B0%95%EC%9D%98
