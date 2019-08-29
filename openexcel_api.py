73b666d8732da8e2bcff262cc392ff04a2ebcedb
yeony402 <yeony402@naver.com>
2019-08-29 15:16:12 +0900

    openexcel_api.py DB에 저장한 데이터를 불러와 엑셀 파일로 변환한다.

--- openexcel_api.py
+++ openexcel_api.py
@@ -1,4 +1,7 @@
 import sys
+import time
+import pandas as pd
+import xlsxwriter
 from PyQt5.QtWidgets import *
 import xlrd
 from urllib.request import quote
@@ -32,6 +35,7 @@ class MyWindow(QWidget):
         fname = QFileDialog.getOpenFileName(self) # 엑셀파일만 읽어오는 것으로 수정해야함 !!!!!!!!!!!
         # 엑셀파일 읽어오기
         wb = xlrd.open_workbook(fname[0])
+        print(fname[0])
         # 모든 시트 가져오기
         sheets = wb.sheets()
         ## 첫번째 시트만 가져오기
@@ -89,14 +93,33 @@ class MyWindow(QWidget):
             list.append(tuple(sub_list))
         print(list)
 
+        now = time.localtime()
+        time_ = "%04d_%02d_%02d_%02d_%02d_%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
+        print(time_)
+
         conn = pymysql.connect(user='root',
                                password='root',
                                host='127.0.0.1',
                                database='bookstore')
         cursor = conn.cursor()
         # 기존에 for문을 이용하면 데이터가 들어올 때마다 mysql에 접속하게 만드는 문제점이 있기 때문에 수정
-        cursor.executemany("INSERT INTO bookinfo (title, author, publisher, price, stock) VALUES (%s, %s, %s, %s, %s)", list)
+        cursor.execute("CREATE TABLE "+ time_+" (번호 int auto_increment primary key, 제목 varchar(200), 저자 varchar(100), 출판사 varchar(50), 가격 int(50), 절판유무 varchar(50))")
+        cursor.executemany("INSERT INTO "+time_+" (제목, 저자, 출판사, 가격, 절판유무) VALUES (%s, %s, %s, %s, %s)", list)
         conn.commit()
+        # cursor.close()
+        # conn.close()
+
+        # db->엑셀로 변환
+        results = pd.read_sql_query('select 제목, 저자, 출판사, 가격, 절판유무 from '+time_, conn)
+        # 데이터프레임 만들기
+        df = pd.DataFrame(results)
+        print(df)
+        # XlsxWriter 엔진으로 Pandas wirter 객체 만들기
+        writer = pd.ExcelWriter(''+time_+'.xlsx', engine='xlsxwriter')
+        # dataframe을 xlsx에 쓰기
+        df.to_excel(writer, sheet_name="Sheet1")
+        #pandas writer 객체 닫기
+        writer.close()
         cursor.close()
         conn.close()
 
@@ -105,3 +128,5 @@ if __name__ == "__main__":
     window = MyWindow()
     window.show()
     app.exec_()
+
+    # CREATE TABLE sdf id int(11) primary key auto_increment, title varchar(200), author varchar(100), publisher varchar(50), price int(50) stock varchar(50)
